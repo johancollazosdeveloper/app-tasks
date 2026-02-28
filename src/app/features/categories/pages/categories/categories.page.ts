14eed05b6fc958d890f54322f9020de08d2eb6d3
johancollazosdeveloper <johan.collazos.developer@gmail.com>
2026-02-28 15:24:32 -0500

    refactor: convertir categories page a standalone y eliminar módulos

--- src/app/features/categories/pages/categories/categories.page.ts
+++ src/app/features/categories/pages/categories/categories.page.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.page.html',
   styleUrls: ['./categories.page.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButton,
+    IonList,
+    IonItem,
+    IonLabel,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CategoriesPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class CategoriesPage {}
--- src/app/features/tasks/pages/tasks/tasks.page.ts
+++ src/app/features/tasks/pages/tasks/tasks.page.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.page.html',
   styleUrls: ['./tasks.page.scss'],
+  standalone: true,
+  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TasksPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class TasksPage {}
